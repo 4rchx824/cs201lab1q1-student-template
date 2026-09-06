11f5a8aad3e94443d92726c758c2fd053f8fc5f9
4rchx824 <archielaw824@gmail.com>
2026-09-06 12:49:33 +0800

    refactor: format array initialization and improve method implementations in NumbersArray

--- NumbersArray.java
+++ NumbersArray.java
@@ -2,24 +2,63 @@ import java.util.*;
 
 public class NumbersArray {
     public static void main(String[] args) {
-        Integer[] input = {1,2,3,4,1,5};
+        Integer[] input = { 1, 2, 3, 4, 1, 5 };
 
         System.out.println("Input : " + Arrays.toString(input));
         System.out.println("Max number : " + findMax(input));
         System.out.println("Duplicate numbers : " + Arrays.toString(findDuplicates(input)));
-        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));       
-        
+        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));
+
         System.out.println();
 
-        input = new Integer[] {1,2,3,4,1,2,5,3};
+        input = new Integer[] { 1, 2, 3, 4, 1, 2, 5, 3 };
 
         System.out.println("Input : " + Arrays.toString(input));
         System.out.println("Max number : " + findMax(input));
         System.out.println("Duplicate numbers : " + Arrays.toString(findDuplicates(input)));
-        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
+        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));
     }
 
     // Write your methods here
-    
-}
+    public static Integer findMax(Integer[] nums) {
+        int max = nums[0];
 
+        for (int i = 1; i < nums.length; i++) {
+            if (max <= nums[i]) {
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            counts.put(nums[i], counts.get(nums[i]) != null ? counts.get(nums[i]) + 1 : 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+
+        return duplicates.toArray(new Integer[0]);
+    }
+
+    public static Integer[] findUnique(Integer[] nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            counts.put(nums[i], counts.get(nums[i]) != null ? counts.get(nums[i]) + 1 : 1);
+        }
+
+        return counts.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .toArray(Integer[]::new);
+    }
+}
